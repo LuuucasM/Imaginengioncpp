efd98f6b7b2800d650d7f998f8f6854efd2077b5
lucasmatth <lm1996kr@naver.com>
2024-03-13 15:01:05 -0400

    Add glfw

--- Imaginengion/Vendor/premake5.lua
+++ Imaginengion/Vendor/premake5.lua
@@ -1,221 +1,100 @@
---[[=====================================
-------------------IMGUI-----------------
-=========================================]]--
-project "Imgui"
-	location "Imgui"
+project "Imaginengion"
 	kind "StaticLib"
 	language "C++"
-	cppdialect "C++20"
-	staticruntime "on"
+	cppdialect "C++17"
+	staticruntime "off"
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+	pchheader "impch.h"
+	pchsource "src/impch.cpp"
 
 	files
 	{
-		"%{prj.name}/imconfig.h",
-		"%{prj.name}/imgui.h",
-		"%{prj.name}/imgui.cpp",
-		"%{prj.name}/imgui_draw.cpp",
-		"%{prj.name}/imgui_internal.h",
-		"%{prj.name}/imgui_widgets.cpp",
-		"%{prj.name}/imstb_rectpack.h",
-		"%{prj.name}/imstb_textedit.h",
-		"%{prj.name}/imstb_truetype.h",
-		"%{prj.name}/imgui_demo.cpp"
+		"src/**.h",
+		"src/**.cpp",
+		"vendor/stb_image/**.h",
+		"vendor/stb_image/**.cpp",
+		"vendor/glm/glm/**.hpp",
+		"vendor/glm/glm/**.inl",
+
+		"vendor/ImGuizmo/ImGuizmo.h",
+		"vendor/ImGuizmo/ImGuizmo.cpp"
 	}
 
-	defines{
-		"_CRT_SECURE_NO_WARNINGS"
+	defines
+	{
+		"_CRT_SECURE_NO_WARNINGS",
+		"GLFW_INCLUDE_NONE"
 	}
+
+	includedirs
+	{
+		"src",
+		"vendor/spdlog/include",
+		"%{IncludeDir.GLFW}",
+		"%{IncludeDir.Glad}",
+		"%{IncludeDir.ImGui}",
+		"%{IncludeDir.glm}",
+		"%{IncludeDir.stb_image}",
+		"%{IncludeDir.entt}",
+		"%{IncludeDir.yaml_cpp}",
+		"%{IncludeDir.ImGuizmo}",
+		"%{IncludeDir.VulkanSDK}"
+	}
+
+	links
+	{
+		"GLFW",
+		"Glad",
+		"ImGui",
+		"yaml-cpp",
+		"opengl32.lib"
+	}
+
+	filter "files:vendor/ImGuizmo/**.cpp"
+	flags { "NoPCH" }
 
 	filter "system:windows"
 		systemversion "latest"
-		
-	filter "system:linux"
-		pic "on"
-		systemversion "latest"
 
-	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "on"
-
-	filter "configurations:Dist"
-        runtime "Release"
-		optimize "on"
---[[=====================================
-------------------GLFW-----------------
-=========================================]]--
-project "GLFW"
-    location "GLFW"
-    kind "StaticLib"
-    language "C"
-	staticruntime "on"
-
-
-    targetdir("bin/" .. outputdir .. "/%{prj.name}")
-    targetdir("bin-int/" .. outputdir .. "/%{prj.name}")
-
-    files{
-		"%{prj.name}/include/GLFW/glfw3.h",
-		"%{prj.name}/include/GLFW/glfw3native.h",
-		"%{prj.name}/src/glfw_config.h",
-		"%{prj.name}/src/context.c",
-		"%{prj.name}/src/init.c",
-		"%{prj.name}/src/input.c",
-		"%{prj.name}/src/monitor.c",
-		"%{prj.name}/src/vulkan.c",
-		"%{prj.name}/src/window.c",
-		"%{prj.name}/src/platform.c",
-		"%{prj.name}/src/null_init.c",
-		"%{prj.name}/src/null_monitor.c",
-		"%{prj.name}/src/null_window.c",
-		"%{prj.name}/src/null_joystick.c"
-    }
-    filter "system:linux"
-        pic "On"
-        systemversion "latest"
-
-        files
-		{
-			"%{prj.name}/src/x11_init.c",
-			"%{prj.name}/src/x11_monitor.c",
-			"%{prj.name}/src/x11_window.c",
-			"%{prj.name}/src/xkb_unicode.c",
-			"%{prj.name}/src/posix_time.c",
-			"%{prj.name}/src/posix_thread.c",
-			"%{prj.name}/src/glx_context.c",
-			"%{prj.name}/src/egl_context.c",
-			"%{prj.name}/src/osmesa_context.c",
-			"%{prj.name}/src/linux_joystick.c"
-		}
-        
 		defines
 		{
-			"_GLFW_X11"
 		}
-
-    filter "system:windows"
-		systemversion "latest"
-
-		files
-		{
-			"%{prj.name}/src/win32_init.c",
-			"%{prj.name}/src/win32_joystick.c",
-			"%{prj.name}/src/win32_monitor.c",
-			"%{prj.name}/src/win32_time.c",
-			"%{prj.name}/src/win32_thread.c",
-			"%{prj.name}/src/win32_window.c",
-			"%{prj.name}/src/wgl_context.c",
-			"%{prj.name}/src/egl_context.c",
-			"%{prj.name}/src/osmesa_context.c",
-			"%{prj.name}/src/win32_module.c"
-		}
-
-		defines 
-		{ 
-			"_GLFW_WIN32",
-			"_CRT_SECURE_NO_WARNINGS"
-		}
-
-    filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "on"
-
-	filter "configurations:Dist"
-        runtime "Release"
-		optimize "on"
---[[=====================================
-------------------GLAD-----------------
-=========================================]]--
-project "Glad"
-    location "Glad"
-    kind "StaticLib"
-    language "C"
-	staticruntime "on"
-
-    targetdir("bin/" .. outputdir .. "/%{prj.name}")
-    targetdir("bin-int/" .. outputdir .. "/%{prj.name}")
-
-    files{
-        "%{prj.name}/include/glad/glad.h",
-        "%{prj.name}/include/KHR/khrplatform.h",
-        "%{prj.name}/src/glad.c"
-    }
-
-    includedirs{
-        "%{prj.name}/include"
-    }
-    filter "system:linux"
-        pic "On"
-        systemversion "latest"
-        
-
-    filter "system:windows"
-        systemversion "latest"
-
-		filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "on"
-
-    filter "configurations:Dist"
-        runtime "Release"
-		optimize "on"
-
---[[=====================================
-------------------YAML-----------------
-=========================================]]--
-project "yaml-cpp"
-    location "yaml-cpp"
-    kind "StaticLib"
-    language "C++"
-	staticruntime "on"
-
-    targetdir("bin/" .. outputdir .. "/%{prj.name}")
-    targetdir("bin-int/" .. outputdir .. "/%{prj.name}")
-
-	defines{
-		"YAML_CPP_STATIC_DEFINE"
-	}
-
-    files{
-        "%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp",
-		"%{prj.name}/include/**.h"
-    }
-
-    includedirs{
-        "%{prj.name}/include"
-    }
-    filter "system:linux"
-		pic "On"
-		systemversion "latest"
-		cppdialect "C++20"        
-
-    filter "system:windows"
-		systemversion "latest"
-		cppdialect "C++20"
 
 	filter "configurations:Debug"
+		defines "IMAGINE_DEBUG"
 		runtime "Debug"
 		symbols "on"
 
+		links
+		{
+			"%{Library.ShaderC_Debug}",
+			"%{Library.SPIRV_Cross_Debug}",
+			"%{Library.SPIRV_Cross_GLSL_Debug}"
+		}
+
 	filter "configurations:Release"
+		defines "IMAGINE_RELEASE"
 		runtime "Release"
 		optimize "on"
 
-    filter "configurations:Dist"
-        runtime "Release"
+		links
+		{
+			"%{Library.ShaderC_Release}",
+			"%{Library.SPIRV_Cross_Release}",
+			"%{Library.SPIRV_Cross_GLSL_Release}"
+		}
+
+	filter "configurations:Dist"
+		defines "IMAGINE_DIST"
+		runtime "Release"
 		optimize "on"
+
+		links
+		{
+			"%{Library.ShaderC_Release}",
+			"%{Library.SPIRV_Cross_Release}",
+			"%{Library.SPIRV_Cross_GLSL_Release}"
+		}
